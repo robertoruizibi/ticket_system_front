0796be2453908671a4fab1da58e07ff145cfd31c
Roberto Ruiz <robertoruizibi@gmail.com>
2022-04-23 18:14:49 +0200

    feat(ticket): changed _tickets to support details and edit

--- pages/tickets/_ticket.vue
+++ pages/tickets/_ticket.vue
@@ -5,8 +5,21 @@ import { checkRol } from '~/utils/common'
 import ticketForm from '~/components/ticketForm'
 
 export default {
+  data() {
+    return {
+      editMode: false
+    }
+  },
   components: {
    ticketForm
+  },
+  methods: {
+    editTicket(){
+      this.editMode = true
+    },
+    detailsTicket(){
+      this.editMode = false
+    }
   }
 }
 </script>
