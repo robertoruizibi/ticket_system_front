d4ed11bb3ab8cfe3a8f069c21aa90afa57341633
Roberto Ruiz <robertoruizibi@gmail.com>
2022-04-23 18:06:42 +0200

    feat(tickets): added CRUD for tickets collection

---
+++ pages/tickets/_ticket.vue
@@ -0,0 +1,12 @@
+<template src="assets/templates/pages/tickets/_ticket.html" />
+
+<script>
+import { checkRol } from '~/utils/common'
+import ticketForm from '~/components/ticketForm'
+
+export default {
+  components: {
+   ticketForm
+  }
+}
+</script>
