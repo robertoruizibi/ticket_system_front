193eaf8487243b459dd1da7bdaaf3279f77c5a95
Roberto Ruiz <robertoruizibi@gmail.com>
2022-04-30 21:23:58 +0200

    feat(login): redirect and remember me

--- pages/index.vue
+++ pages/index.vue
@@ -13,7 +13,8 @@ export default {
       email: '',
       password: '',
       loginError: false,
-      loginMessage: 'Usuario o contraseña incorrectos'
+      loginMessage: 'Usuario o contraseña incorrectos',
+      rememberMe: false
     }
   },
   computed: {
@@ -22,6 +23,19 @@ export default {
     })
   },
   methods: {
+    checkRememberMeSubmit(){
+     if (this.rememberMe) {
+       localStorage.setItem('userRemember', this.email)
+     } else {
+       localStorage.setItem('userRemember', '')
+     }
+    },
+    checkRememberMeOnLoad(){
+      this.email = localStorage.getItem('userRemember')
+      if (this.email !== ''){
+        this.rememberMe = true
+      }
+    },
     async loginUser(){
       let response = await login(this, this.email, this.password)
       if (!isObjEmpty(get(response, 'error', {}))) {
@@ -29,6 +43,7 @@ export default {
       }else {
         this.loginError = false
         if (get(response, 'data.user.rol', '') === 'empresa') {
+          this.checkRememberMeSubmit()
           this.$router.push({ path: routes.users })
         }else {
           this.$router.push({ path: routes.tickets })
@@ -38,6 +53,7 @@ export default {
   },
   mounted(){
     checkRolInLogin(this)
+    this.checkRememberMeOnLoad()
   }
 }
 </script>
